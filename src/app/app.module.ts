f1d449c0a9a7fcf77fc001dd015e96516a0be210
Camilla Freitas <camirsfreitas@gmail.com>
2020-10-08 16:56:41 -0300

    Lista de professores adicionada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListaProfessoresComponent } from './lista-professores/lista-professores.component';
+import { ListaCursosComponent } from './lista-cursos/lista-cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProfessoresComponent,
+    ListaCursosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
